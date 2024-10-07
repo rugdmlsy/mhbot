8ea6b73d458ede3566b1e7581da55afb43ad41f6
xyc <1598027117@qq.com>
2024-10-07 16:44:24 +0800

    修复重复复读

---
+++ mhbot/plugins/auto-plus-one/README.md
@@ -0,0 +1,62 @@
+
+<h1 align="center">🐣🐤一个仅有40行代码的复读姬 ✨</h1>
+<p align="center">
+_✨ 只是一个复读姬：支持群聊白名单、文本复读、图片表情复读、甚至是转发和分享的复读 ✨_
+</p>
+<p align="center">
+  <a href="https://raw.githubusercontent.com/cscs181/QQ-Github-Bot/master/LICENSE">
+    <img src="https://img.shields.io/github/license/cscs181/QQ-Github-Bot.svg" alt="license">
+  </a>
+  <a href="https://pypi.python.org/pypi/nonebot-plugin-analysis-bilibili">
+    <img src="https://img.shields.io/pypi/v/nonebot-plugin-analysis-bilibili.svg" alt="pypi">
+  </a>
+  <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="python">
+</p>
+
+
+## 快速安装
+
+第一步：右上角 ↗ 点个不要钱的 star 吧，这是不断维护更新的动力。
+
+### nb-cli
+
+```shell
+nb plugin install nonebot-plugin-plus-one
+```
+### pip
+
+```shell
+pip install nonebot-plugin-plus-one
+```
+
+### git
+
+```shell
+cd /your-nonebot-project-home/plugins/
+git clone https://github.com/yejue/nonebot-plugin-plus-one.git
+```
+
+## 示例
+```text
+[群1]: 你好
+[群1]: 你好
+[机器人]: 你好
+
+[群2]: 表情
+[群2]: 表情
+[机器人]: 表情
+
+[群2]: 分享音乐
+[群2]: 分享音乐
+[机器人]: 分享音乐
+```
+
+## 配置
+```text
+# 匹配优先级
+PLUS_ONE_PRIORITY = 10
+
+# 群聊或私聊白名单，单个或多个示例，下面任选其一，可填入群 QQ 号或个人 QQ 号
+PLUS_ONE_WHITE_LIST = ["10000000"]
+PLUS_ONE_WHITE_LIST = ["10000000", "1000000"]
+```
---
+++ mhbot/plugins/auto-plus-one/handler.py
@@ -0,0 +1,48 @@
+from nonebot.plugin import on_message
+from nonebot.adapters import Event, Message, Bot
+from nonebot_plugin_session import extract_session, SessionIdType
+
+from .config import config
+
+plus = on_message(priority=config.plus_one_priority, block=False)
+msg_dict = {}
+
+
+def is_equal(msg1: Message, msg2: Message):
+    """判断是否相等"""
+    if len(msg1) == len(msg2) == 1 and msg1[0].type == msg2[0].type == "image":
+        if msg1[0].data["file_size"] == msg2[0].data["file_size"]:
+            return True
+    if msg1 == msg2:
+        return True
+
+
+@plus.handle()
+async def plush_handler(bot: Bot, event: Event):
+    global msg_dict
+
+    session = extract_session(bot, event)
+    group_id = session.get_id(SessionIdType.GROUP).split("_")[-1]
+    if group_id not in config.plus_one_white_list:
+        return
+
+    # 获取群聊记录
+    text_list = msg_dict.get(group_id, None)
+    if not text_list:
+        text_list = []
+        msg_dict[group_id] = text_list
+
+    # 获取当前信息
+    msg = event.get_message()
+
+    try:
+        if not is_equal(text_list[-1], msg):
+            text_list = []
+            msg_dict[group_id] = text_list
+    except IndexError:
+        pass
+
+    text_list.append(msg)
+
+    if len(text_list) == 2:
+        await plus.send(msg)
